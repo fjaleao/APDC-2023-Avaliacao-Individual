23478a7b8ede99e5c87623c7995cc948dfc81fb2
f-leao <fjaleao@gmail.com>
2023-04-12 23:45:00 +0100

    progress with ajax

---
+++ src/main/java/pt/unl/fct/di/apdc/firstwebapp/util/UserType.java
@@ -0,0 +1,39 @@
+package pt.unl.fct.di.apdc.firstwebapp.util;
+
+/**
+ * @author f.leao
+ * 
+ * define o privilégio para autorização de execução de operações na aplicação
+ */
+public enum UserType {
+
+    /**
+     * 
+     * <h1>Superuser</h1>
+     * They may:<p>
+     * > Change any registered user`s <b>ROLE</b>;<p>
+     * > Toggle any registered user`s {@code status};<p>
+     * > Remove any user, no matter their <b>ROLE</b>;<p>
+     * > List any registered users` <b>attributes</b> and their {@code status};<p>
+     * > Modify GS, GBO or USER users` <b>attributes</b>, except their {@code username};
+     * 
+    */
+    SU,
+    
+    /**
+     * <pre>
+     * 
+     * <code>System Manager</code> may:
+     * > Change USERs to GBOs;
+     * > Toggle GBO and GA users` status;
+     * > Remove USER, GBO and GA users;
+     * > List USER and GBO users` <b>attributes</b> and their status, no matter their VISIBILITY or status;
+     * > Modify GS, GBO or USER user`s <b>attributes</b>, except their username;
+     * 
+     * </pre>
+    */
+    GS,
+    GA,
+    GBO,
+    USER;
+}
